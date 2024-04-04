e6ac68c0b40cf5f27e2fc057149439cc2bd4cba6
Jonathan Hodak <10237065+jhodak@users.noreply.github.com>
2024-04-04 00:05:45 -0400

    fixes

--- utils/apiValidation.js
+++ utils/apiValidation.js
@@ -5,10 +5,8 @@ const { handleErrors } = require("../utils/utils");
 const validateApiKey = (req, res, next) => {
   if (
     process.env.NODE_ENV === "production" &&
-    !req.headers["x-rapidapi-proxy-secret"] &&
-    !req.headers["x-rapidapi-secret"]
+    !req.headers["x-rapidapi-proxy-secret"]
   ) {
-    console.log("401 for entire api key validation around all apis");
     handleErrors(res, 401);
     return;
   }
