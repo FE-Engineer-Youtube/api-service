eece1e8d0d9aa8fbb3ba888bdf13636f9320a056
Jonathan Hodak <10237065+jhodak@users.noreply.github.com>
2024-04-08 01:47:34 -0400

    add health

--- routes/health.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Endpoint to get WHOIS data for a domain (GET)
-router.get("/", async (req, res) => {
-  return res.status(200).send({ message: "success" });
-});
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const app = express();
 const validateApiKey = require("./utils/apiValidation");
 const cors = require("cors");
+const serverHealth = require("server-health");
 
+const app = express();
 // Parse JSON-encoded request bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +20,6 @@ const apiRoutes = {
   "/qr": require("./routes/qr"),
   "/ping": require("./routes/ping"),
   "/whois": require("./routes/whois"),
-  "/health": require("./routes/health"),
   "/dns": require("./routes/dns"),
 };
 
@@ -28,6 +28,8 @@ for (const [route, handler] of Object.entries(apiRoutes)) {
 }
 
 // Other non-API routes can be defined here...
+
+serverHealth.exposeHealthEndpoint(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`API's are listening on port ${PORT}.`));
